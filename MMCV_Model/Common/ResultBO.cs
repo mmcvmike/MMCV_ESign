d9e61621de66a4f560ce58e83d96571d706ea05e
tangdv <Do.Van.Tang@mektec.nokgrp.com>
2023-07-31 13:00:49 +0700

    TangDV: update remove background

---
+++ MMCV_Model/Common/ResultBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMCV_Model.Common
+{
+    public class ResultBO
+    {
+        public bool status { get; set; }
+        public string message { get; set; }
+        public string data { get; set; }
+    }
+}
